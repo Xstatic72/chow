1c426c0fc09a8d77873256325bd06b61f0fb92de
Onyedikachukwu Okeke <xstati72@gmail.com>
2026-05-11 18:20:34 +0100

    style: adjust Card component layout and enhance name text styling for improved readability

--- app/components/Card.vue
+++ app/components/Card.vue
@@ -1,13 +1,13 @@
 <template>
     <div>
-        <div class="relative w-[350px] h-[400px] bg-cover p-4 bg-center content-end text-left rounded-xl hover:translate-y-[-10px] hover:shadow-lg hover:-rotate-1 transition-all hover:shadow-[var(--color-btn-hover)] duration-500" :style="{ backgroundImage: `url('${image}')` }">
+        <p class="text-xl text-[var(--color-primary)] font-bold pb-2">{{ title }}</p>
+        <div class="relative w-[350px] h-[250px] bg-cover p-4 bg-center content-end text-left rounded-xl hover:translate-y-[-10px] hover:shadow-lg hover:-rotate-1 transition-all hover:shadow-[var(--color-btn-hover)] duration-500" :style="{ backgroundImage: `url('${image}')` }">
             <div class="absolute inset-0 bg-black bg-opacity-30 rounded-xl"></div>
             <div class="relative z-10">
-                <p class="text-white text-2xl font-bold pb-2">{{ name }}</p>
+                <p class="text-white text-2xl font-bold max-w-sm pb-2">{{ name }}</p>
             </div>
             
         </div>
-        <p class="text-xl text-[var(--color-primary)] font-bold pt-4">{{ title }}</p>
     </div>
 </template>
 
