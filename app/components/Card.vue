bad6897ab314cb9683762b7f22604d7d4a6ba2fe
Onyedikachukwu Okeke <xstati72@gmail.com>
2026-05-12 18:40:36 +0100

    style: adjust hover translation effect on Card component for improved UI interaction

--- app/components/Card.vue
+++ app/components/Card.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <p class="text-xl text-[var(--color-primary)] font-bold pb-2">{{ title }}</p>
-        <div class="relative w-[350px] h-[250px] bg-cover p-4 bg-center content-end text-left rounded-xl hover:translate-y-[-10px] hover:-rotate-1 transition-all hover:shadow-lg hover:shadow-green-500/40 duration-500" :style="{ backgroundImage: `url('${image}')` }">
+        <div class="relative w-[350px] h-[250px] bg-cover p-4 bg-center content-end text-left rounded-xl hover:translate-y-[-8px] hover:-rotate-1 transition-all hover:shadow-lg hover:shadow-green-500/40 duration-500" :style="{ backgroundImage: `url('${image}')` }">
             <div class="absolute inset-0 bg-black bg-opacity-30 rounded-xl"></div>
             <div class="relative z-10">
                 <p class="text-white text-2xl font-bold max-w-sm pb-2">{{ name }}</p>
