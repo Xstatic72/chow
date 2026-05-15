761cd726a0a3f88e60362f9e3c458cc4bc6970bd
Onyedikachukwu Okeke <xstati72@gmail.com>
2026-05-15 17:15:57 +0100

    style: update user options menu visibility logic for improved user experience

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -11,7 +11,7 @@
             leave-from-class="opacity-100 scale-100 translate-y-0"
             leave-to-class="opacity-0 scale-95 -translate-y-5"
             >
-            <div v-if="isUserOptionsOpen" role="menu" aria-label="User menu" @keydown.esc="isUserOptionsOpen = false" class="absolute top-16 -left-11 z-50 min-w-[12rem] w-48 backdrop-blur-md bg-green-500/60 text-white rounded-lg py-2 shadow-xl text-sm">
+            <div v-if="isUserOptionsOpen && !isMobileExpanded" role="menu" aria-label="User menu" @keydown.esc="isUserOptionsOpen = false" class="absolute top-16 -left-11 z-50 min-w-[12rem] w-48 backdrop-blur-md bg-green-500/60 text-white rounded-lg py-2 shadow-xl text-sm">
                 <NuxtLink @click="navigateTo('/dashboard/saved')" role="menuitem" tabindex="0" class="h-12 flex items-center px-4 hover:bg-emerald-300 focus:bg-emerald-300 focus:outline-none"><Heart class="mr-2 h-5 w-5 text-red-400" />Saved recipes</NuxtLink>
                 <NuxtLink @click="isAboutOpen=true" role="menuitem" tabindex="0" class="h-12 flex items-center px-4 hover:bg-emerald-300 focus:bg-emerald-300 focus:outline-none"><Info class="mr-2 h-5 w-5" />About</NuxtLink>
                 <NuxtLink role="menuitem" tabindex="0" @click="logout" class="h-12 flex items-center px-4 hover:bg-emerald-300 focus:bg-emerald-300 focus:outline-none"><LogOut class="mr-2 h-5 w-5" />Logout</NuxtLink>
